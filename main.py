0b90ec92ccbfdeb1fd0dcf5a19d2f7a93250f953
sam-dp <cloudysaga@gmail.com>
2023-07-06 02:19:12 -0700

    Basic scraping implemented

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
 
-r = requests.get('https://jojowiki.com/Art_Gallery#2021-2025-0')
+# GET Request
+URL =  'https://jojowiki.com/Art_Gallery#2021-2025-0'
+page = requests.get( URL )
+
+# Check for successful status code (200)
+print(page.status_code)
+
+# HTML Parser
+soup = BeautifulSoup(page.content, 'html.parser')
+
+content = soup.find_all("table", {"class":"diamonds volume"})
+
+print(content)
 
