7476af637c4b6184b2cff3681aa63a62f855e4c1
sam-dp <cloudysaga@gmail.com>
2023-07-06 19:12:26 -0700

    Basic HTML Parsing

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 # GET Request
 URL =  'https://jojowiki.com/Art_Gallery#2021-2025-0'
 page = requests.get( URL )
 
+
 # Check for successful status code (200)
 print(page.status_code)
 
+
 # HTML Parser
 soup = BeautifulSoup(page.content, 'html.parser')
+div = soup.find("div", {"class":"phantom-blood-tabs"})
+entries = div.find_all("table", {"class":"diamonds volume"})
 
-content = soup.find_all("table", {"class":"diamonds volume"})
 
-print(content)
+
+
 
